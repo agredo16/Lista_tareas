3a30ada8b8189856952a687c637a1b2f8909a10d
daniel <danielortizd7mail.com>
2024-11-12 20:11:26 -0500

    segundo commit cambio-estado

--- cambioestado.ts
+++ cambioestado.ts
@@ -1,8 +1,8 @@
 // Función para cambiar el estado de una tarea específica usando una función flecha.
 // La función toma como parámetros el array de tareas y el id de la tarea que se quiere cambiar.
 // Usa .map() para recorrer cada tarea y cambia el estado de la tarea que coincide con el id.
-var cambiarEstadoTarea = function (tareas, id) {
-    return tareas.map(function (tarea) {
-        return tarea.id === id ? __assign(__assign({}, tarea), { completada: !tarea.completada }) : tarea;
-    });
+const cambiarEstadoTarea = (tareas: { id: number, nombre: string, completada: boolean }[], id: number) => {
+    return tareas.map(tarea =>
+        tarea.id === id ? { ...tarea, completada: !tarea.completada } : tarea
+    );
 };
