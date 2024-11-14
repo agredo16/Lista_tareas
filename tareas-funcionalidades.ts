845ce4709430ddecb31e49ccb8b0579dea0bc652
castrokarolyisela@gmail.com <castrokarolyisela@gmail.com>
2024-11-13 19:30:14 -0500

    codigo modificado

--- tareas-funcionalidades.ts
+++ tareas-funcionalidades.ts
@@ -1,14 +1,9 @@
-interface Tarea {
-    id: number;
-    nombre: string;
-    completado: boolean;
-
-}
-
-const listaTarea: Tarea [] = [
+let tareas = [
     {id:1, nombre: "Practicar TypeScritp", completado: true},
     {id: 2, nombre:"Practicar English", completado: false},
     {id:3, nombre:"Practicar ejercicios de logica",completado:false}
+
 ];
 
-export {Tarea, listaTarea};
+
+
